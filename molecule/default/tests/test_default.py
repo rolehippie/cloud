f3f772eed8b8aa55645c0e33422744338c0d9e6e
Thomas Boerger <thomas@webhippie.de>
2025-09-18 11:31:36 +0200

    ci: add basic tests for install

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -6,5 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_dummy(host):
-    assert True
+@pytest.mark.parametrize("executable", [
+    "/usr/bin/s5cmd",
+    "/usr/bin/eksctl",
+    "/usr/bin/hcloud",
+    "/usr/bin/az"
+])
+def test_is_installed(host, executable):
+    file = host.file(executable)
+    assert file.is_file
